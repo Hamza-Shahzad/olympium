3587a0d5a8826e3c817b4130eb5041bd04daa3ce
Hamza Shahzad <Hamza-Shahzad@users.noreply.github.com>
2021-01-01 12:45:02 -0500

    Added js files

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_zbu9Au6EF', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'b1qae4un9cfc6mnd717hj0g7e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://7zks55d301.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
